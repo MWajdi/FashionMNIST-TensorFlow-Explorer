c24b2bb9982ed5a2e41ac88461a1954dcbca2a9e
Wajdi <wajdi.maatouk54@gmail.com>
2024-02-13 21:55:49 +0100

    improved model architecture using ConvNets

--- training_logic.py
+++ training_logic.py
@@ -15,7 +15,7 @@ def visualize_image(index):
 
 class MyCallback(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if logs.get('accuracy') > 0.9:
+        if logs.get('accuracy') > 0.99:
             print("\n Accuracy of 90% reached")
             self.model.stop_training = True
 
@@ -24,7 +24,11 @@ callbacks = MyCallback()
 
 
 model = tf.keras.Sequential( [
-    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Flatten(),
     keras.layers.Dense(512, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
@@ -35,6 +39,6 @@ test_images = test_images / 255.0
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(train_images, train_labels, epochs=99, callbacks = [callbacks])
+model.fit(train_images, train_labels, epochs=10, callbacks = [callbacks])
 
 model.evaluate(test_images, test_labels)
