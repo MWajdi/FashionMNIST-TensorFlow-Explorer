ca7cd7a56c8e689855545fec93f30027763a42a9
Wajdi <wajdi.maatouk54@gmail.com>
2024-02-13 04:55:14 +0100

    training logic and visualization tools implemented

--- fashion_mnist_data.py
+++ fashion_mnist_data.py
@@ -2,5 +2,3 @@ import tensorflow as tf
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-print(min(train_labels))
---
+++ training_logic.py
@@ -0,0 +1,40 @@
+from fashion_mnist_data import train_images, train_labels, test_images, test_labels, fashion_mnist
+import tensorflow as tf
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize_image(index):
+    np.set_printoptions(linewidth=320)
+
+    print(f'LABEL: {train_labels[index]}')
+    print(f'\nIMAGE PIXEL ARRAY:\n {train_images[index]}')
+
+    plt.imshow(train_images[index], cmap="Greys")
+    plt.show()
+
+class MyCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') > 0.9:
+            print("\n Accuracy of 90% reached")
+            self.model.stop_training = True
+
+callbacks = MyCallback()
+
+
+
+model = tf.keras.Sequential( [
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(512, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=99, callbacks = [callbacks])
+
+model.evaluate(test_images, test_labels)
