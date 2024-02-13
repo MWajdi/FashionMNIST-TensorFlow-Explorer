3b1c8d7c33a2cdda71e2626b4d81ae072744b619
Wajdi <wajdi.maatouk54@gmail.com>
2024-02-13 21:56:13 +0100

    improved model architecture using ConvNets

--- training_logic.py
+++ training_logic.py
@@ -24,9 +24,9 @@ callbacks = MyCallback()
 
 
 model = tf.keras.Sequential( [
-    keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28,28,1)),
+    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
     keras.layers.MaxPooling2D((2,2)),
-    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
     keras.layers.MaxPooling2D((2,2)),
     keras.layers.Flatten(),
     keras.layers.Dense(512, activation=tf.nn.relu),
