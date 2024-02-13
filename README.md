b30a372698e8c687eeca00599373b1a86ddceb6d
Wajdi <wajdi.maatouk54@gmail.com>
2024-02-13 03:07:32 +0100

    data loading complete

---
+++ README.md
@@ -0,0 +1,1 @@
+# FashionMNIST-TensorFlow-Explorer
---
+++ fashion_mnist_data.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+print(min(train_labels))
